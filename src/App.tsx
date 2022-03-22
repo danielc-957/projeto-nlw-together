72866bb2caa571ca20f5c9a199d83c8a8b000bf7
Daniel Carvalho <danielcarvalho957@gmail.com>
2022-03-22 18:09:55 -0300

    Tela Home e tela New Room criadas. Modulo de Roteamento e navegacao criados

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import { Button } from './components/Button';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+
 function App() {
   return (
-    <Button />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Home/> } />
+        <Route path="/rooms/new" element={ <NewRoom/> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,11 @@
-import { useState } from "react";
+import {ButtonHTMLAttributes} from 'react';
+import '../styles/button.css';
 
-export function Button(){
-    const [contador,setContador] = useState(0);
-    
-    function increment (){
-        setContador(contador + 2);
-        
-    }
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Button(props: ButtonProps){
+
     return (
-        <button onClick={increment}> {contador} </button>
+        <button className="button" {...props} />
     );
 }
